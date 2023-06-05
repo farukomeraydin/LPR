cbb1f94c3a1295f752d630c9675f3b63a44fdac4
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2023-06-05 22:31:30 +0300

    main

--- Detection/main.py
+++ Detection/main.py
@@ -3,55 +3,82 @@ import numpy as np
 from time import time
 from windowcapture import WindowCapture
 from vision import Vision
-from hsvfilter import HsvFilter
+import matplotlib.pyplot as plt
 
-#WindowCapture.list_window_names()
+def get_time_frame(hour=0, min=0, sec=0, frame_rate=None):
+    specific_frame = (hour * 3600 + min * 60 + sec) * frame_rate
+    return int(specific_frame)
 
-
+"""
 wincap = WindowCapture()
-cap = cv.VideoCapture('driving.mp4')
-vision_lp = Vision('hsv1.jpg')
-vision_lp.init_control_gui()
+cap = cv.VideoCapture('driving2_1080p.mkv')
 
-hsv_filter = HsvFilter(0, 0, 115, 179, 255, 255, 0, 0, 20, 0)
+frame_rate = cap.get(cv.CAP_PROP_FPS)    
+    
+frame_num = get_time_frame(hour=0, min=3, sec=10, frame_rate=frame_rate)
+
+cap.set(1, frame_num)
+"""
+cascade_lp = cv.CascadeClassifier('cascade_1200pos_2600neg_15stage_72_24/cascade.xml')
+cap = cv.VideoCapture('driving4.mp4')
+vision_lp = Vision(None)
 loop_time = time()
-_, start_frame = cap.read()
+from time import sleep
 i = 1
+
+#cv.namedWindow("Matches", cv.WINDOW_NORMAL)
+#cv.resizeWindow("Matches", 480, 270)
+kernel = cv.getStructuringElement(cv.MORPH_RECT,(1, 1))
+ret, frame = cap.read()
+
+interval = frame.shape[1] // 36
+dead_zone = int(interval * 7.5)
+k = 0
 while True:
     ret, screenshot = cap.read()
-    #screenshot = wincap.get_screenshot()
-    processed_image = vision_lp.apply_hsv_filter(screenshot)  
-    edges_image = vision_lp.apply_edge_filter(processed_image)
-    #rectangles = vision_lp.find(processed_image, 0.46)
-    #output_image = vision_lp.draw_rectangles(screenshot, rectangles)
+    screenshot = screenshot[:, dead_zone:screenshot.shape[1] - dead_zone, :]
+    rectangles = cascade_lp.detectMultiScale(screenshot, minNeighbors=3, minSize=(24, 72), maxSize=(24, 72))
+    if len(rectangles):
+        for i in range(len(rectangles)):
+            x = rectangles[i][0]
+            y = rectangles[i][1]
+            w = rectangles[i][2]
+            h = rectangles[i][3]
+            crop = screenshot[y:y + h, x:x + w]
+            gray = cv.cvtColor(crop, cv.COLOR_BGR2GRAY)
+            th1 = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+            th2 = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+            
+            #eroded_image = cv.erode(im_bw, kernel, iterations=1)
+            #dilated_image = cv.dilate(im_bw, kernel, iterations=1)
+            opening = cv.morphologyEx(th1, cv.MORPH_OPEN, kernel)
+            closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel)
+            
+            plt.figure()
+            plt.imshow(closing, cmap='gray')
+            plt.show()
+            cv.imwrite(f'sample/{k}.jpg', closing)
+            cv.imwrite(f'sample/gray_{k}.jpg', gray)
+            k += 1
+            
+    detection_image = vision_lp.draw_rectangles(screenshot, rectangles)
     
-    keypoint_image = edges_image
-    
-    x, w, y, h = [200, 1130, 70, 750]
-    keypoint_image = keypoint_image[y:y+h, x:x+w]
-    kp1, kp2, matches, match_points = vision_lp.match_keypoints(keypoint_image)
-    match_image = cv.drawMatches(vision_lp.needle_img, kp1, keypoint_image, kp2, matches, None)
-    
-    if match_points:
-        # find the center point of all the matched features
-        center_point = vision_lp.centeroid(match_points)
-        # account for the width of the needle image that appears on the left
-        center_point[0] += vision_lp.needle_w
-        # drawn the found center point on the output image
-        match_image = vision_lp.draw_crosshairs(match_image, [center_point])
+    cv.imshow('Matches', detection_image)
 
-    
-    
-    cv.imshow('Keypoint Search', match_image)
-    cv.imshow('Processed', processed_image)
-    cv.imshow('Edges', edges_image)
-    #cv.imshow('Matches', output_image)
-    
-    print(f'FPS {1 / (time() - loop_time)}')
+    print(f'FPS {1 / (time() - loop_time + 0.000001)}')
     loop_time = time()
     
-    if cv.waitKey(1) == ord('q'):
+    key = cv.waitKey(1)
+    #sleep(0.01)
+    
+    if key == ord('q'):
         break
+    #elif key == ord('f'):
+    #    cv.imwrite(f'positive2/p{loop_time}.jpg', screenshot)
+    #elif key == ord('d'):
+    #    cv.imwrite(f'negative/{loop_time}.jpg', screenshot)
+    
+    
 
     
 
