1f548695ee00718dbdd06d0509e7cf6e5c552954
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2023-06-06 16:35:26 +0300

    Update LPR.py

--- LPR.py
+++ LPR.py
@@ -6,8 +6,6 @@ from windowcapture import WindowCapture
 from vision import Vision
 from time import time, sleep
 
-os.chdir(r'C:\Users\faruk\OneDrive\Masaüstü\haar')
-
 
 class LPR:
     dim1 = 24
@@ -199,43 +197,3 @@ class LPR:
         cv.waitKey(0)
         cv.destroyAllWindows()
     
-
-########DETECTION ON IMAGE##########
-
-lpr = LPR('cascade_1200pos_2600neg_15stage_72_24/cascade.xml')
-input_image = lpr.read_image('home.jpg')
-
-di = lpr.detect_image(input_image, 25)
-lpr.display_img(di, 'Matches')
-####################################
-
-
-########DETECTION ON VIDEO##########
-
-lpr = LPR('cascade_1200pos_2600neg_15stage_72_24/cascade.xml')
-kernel = cv.getStructuringElement(cv.MORPH_RECT,(1, 1))
-lpr.detect_video('driving4.mp4', kernel, maxSize=(24, 72), isWrite=False, printFPS=True, isDisplay=False)
-
-####################################
-
-
-###########SPLIT FOR OCR############
-
-lpr = LPR(None)
-
-img = lpr.read_image('sample/111.jpg', isGray=True)
-lpr.display_img(img, 'CROPPED')
-
-orig, inv = lpr.inverse_threshold(img)
-
-lpr.floodfill(inv, 0, 5)
-lpr.display_plt(inv, 'filled')
-
-lpr.floodfill(inv, 23, 30)
-lpr.display_plt(inv, 'filled')
-
-characters = lpr.split(orig, inv)
-
-for i in characters:
-    lpr.display_plt(i, 'Char')
-####################################
